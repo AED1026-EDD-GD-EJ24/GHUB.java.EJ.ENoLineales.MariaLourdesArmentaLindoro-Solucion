071d967195d71de4dbbc85de55cf3182203aea7a
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-05-02 16:54:30 +0000

    Actualizacion Arbol Binario

---
+++ arbolBinario/ArbolBinario.java
@@ -0,0 +1,18 @@
+package arbolBinario;
+public class ArbolBinario{
+    private Nodo raiz;
+    public ArbolBinario(){
+        this.raiz = null;
+    }
+    public ArbolBinario(Nodo raiz){
+        this.raiz = raiz;
+    
+    }
+    public Nodo getRaiz(){
+        return this.raiz;
+    }
+    public void setRaiz(Nodo raiz){
+        this.raiz = raiz;
+    }
+    
+}
